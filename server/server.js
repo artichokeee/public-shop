1fb01cec87900de5df9d173e374968b3b73954fb
artichokeee <rusau.qalife@gmail.com>
2024-03-05 19:35:24 +0300

    fixed baseurl location in server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 const jwt = require("jsonwebtoken");
 const path = require("path");
-const baseUrl = require("../../config/baseUrl");
+const baseUrl = require("../config/baseUrl");
 
 // Читаем содержимое файла с секретным ключом
 const configPath = path.join(__dirname, "config.json");
