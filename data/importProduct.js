50ab3fdbf452a7da92f5d1b31722b4a6bdb7b68f
artichokeee <rusau.qalife@gmail.com>
2024-03-05 18:11:37 +0300

    updated gitignore

--- data/importProduct.js
+++ data/importProduct.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 
 async function insertProducts() {
   // Замените следующие значения на реальные параметры подключения к вашей MySQL базе данных
-  const connection = await mysql.createConnection({
-    host: "127.0.0.1",
-    port: "3306",
-    user: "root",
-    password: "root1234@",
-    database: "web_shop",
-  });
+  // const connection = await mysql.createConnection({
+  //   host: "127.0.0.1",
+  //   port: "3306",
+  //   user: "root",
+  //   password: "root1234@",
+  //   database: "web_shop",
+  // });
 
   //   await connection.execute("TRUNCATE TABLE products");
 
