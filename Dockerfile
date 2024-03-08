a6d94f8a5c10efdbc43163d944bf74ec9b29c12c
artichokeee <rusau.qalife@gmail.com>
2024-03-09 00:35:45 +0300

    updated docker files and added cypress tests

--- Dockerfile
+++ Dockerfile
@@ -16,17 +16,5 @@ COPY . .
 # Указываем порт, который будет прослушивать приложение
 EXPOSE 3000
 
-# Устанавливаем переменную окружения BASE_URL
-ENV BASE_URL=http://localhost
-
-# Изменяем конфигурацию БД перед запуском
-RUN sed -i "s#host: \"185.251.91.112\"#host: \"mysql\"#g" ./config/dbConfig.js
-
-# Изменяем файлы HTML перед запуском
-RUN sh -c "find . -name '*.html' -type f -exec sed -i 's#window.baseUrl = \"\";#window.baseUrl = \"${BASE_URL}\";#g' {} +"
-
-# Изменяем файл server.js перед запуском
-RUN sed -i "s#const baseUrl = 'http://localhost';#const baseUrl = '${BASE_URL}';#g" ./server/server.js
-
 # Команда, которая будет запущена при запуске контейнера
 CMD ["node", "./server/server.js"]
--- cypress/e2e/gui/app.cy.js
+++ cypress/e2e/gui/app.cy.js
@@ -1,51 +1,100 @@
 describe("Page GUI Tests", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:3000");
+    cy.visit("/");
+    cy.clearLocalStorage();
   });
 
   it("Should display the product list", () => {
-    cy.get("#product-list").should("exist");
-    cy.get(".product").should("have.length.above", 0);
+    cy.get("#product-list").children().should("have.length.greaterThan", 0);
   });
 
-  it("Should apply filters correctly", () => {
-    // Проверяем наличие элементов фильтрации
-    cy.get("#min-price").should("exist");
-    cy.get("#max-price").should("exist");
-    cy.get("#category").should("exist");
-    cy.get("#manufacturer").should("exist");
-    cy.get("#free-shipping").should("exist");
+  it("Should apply filters correctly and reflect in the UI", () => {
+    const minPrice = "500";
+    const maxPrice = "1000";
+    const category = "Laptops";
+    const manufacturer = "Apple";
+    const freeShipping = true;
 
-    // Применяем фильтры
-    cy.get("#min-price").type("500");
-    cy.get("#max-price").type("1000");
-    cy.get("#category").select("Laptops");
-    cy.get("#manufacturer").select("Samsung");
-    cy.get("#free-shipping").check();
+    cy.get("#min-price").type(minPrice);
+    cy.get("#max-price").type(maxPrice);
+    cy.get("#category").select(category);
+    cy.get("#manufacturer").select(manufacturer);
+    if (freeShipping) {
+      cy.get("#free-shipping").check();
+    }
 
-    // Нажимаем кнопку применения фильтров
     cy.get("#apply-filters").click();
 
-    // Проверяем, что список продуктов изменился после применения фильтров
-    cy.get(".product").should("have.length.above", 0);
+    // Ожидаем, что список товаров обновится и будет содержать хотя бы один товар
+    cy.get("#product-list").children().should("have.length.greaterThan", 0);
+    cy.wait(1000);
+
+    // Предполагаем, что имя товара содержится внутри элемента h2 каждой карточки товара
+    cy.get("#product-list")
+      .children()
+      .each(($el) => {
+        cy.wrap($el)
+          .find("h2") // Используйте селектор, который точно указывает на элемент с именем товара
+          .should("contain.text", manufacturer);
+      });
   });
 
-  it("Should reset filters correctly", () => {
-    // Применяем некоторые фильтры
-    cy.get("#min-price").type("10");
-    cy.get("#max-price").type("50");
-    cy.get("#category").select("Laptops");
-    cy.get("#manufacturer").select("Samsung");
+  it("Should reset filters correctly and update the product list accordingly", () => {
+    // Применяем фильтры
+    cy.get("#min-price").type("100");
+    cy.get("#max-price").type("200");
+    cy.get("#category").select("Phones");
+    cy.get("#manufacturer").select("Xiaomi");
     cy.get("#free-shipping").check();
 
-    // Нажимаем кнопку сброса фильтров
     cy.get("#reset-filters").click();
 
-    // Проверяем, что значения фильтров сброшены
     cy.get("#min-price").should("have.value", "");
     cy.get("#max-price").should("have.value", "");
     cy.get("#category").should("have.value", "");
     cy.get("#manufacturer").should("have.value", "");
     cy.get("#free-shipping").should("not.be.checked");
+
+    cy.get("#product-list").children().should("have.length.greaterThan", 0);
+    // Проверка, что список товаров вернулся к исходному виду, может потребовать мокирования API
   });
+
+  it("Should display correct pagination", () => {
+    // Проверяем, что пагинация отображается корректно
+    cy.get("#pagination").children().should("have.length.greaterThan", 1);
+    // Кликаем по второй странице и проверяем, что список товаров обновляется
+    cy.get("#pagination").children().eq(1).click();
+    cy.get("#product-list").children().should("have.length.greaterThan", 0);
+    // Возможны дополнительные проверки для убеждения в корректности пагинации
+  });
+  it.only("Should sort products correctly from A to Z and Z to A", () => {
+    cy.get("#sort-order").select("name-asc");
+    // Дополнительная проверка сортировки от A до Z
+    checkSorting("asc", "h2");
+
+    cy.wait(1000); // Замените это на более подходящее ожидание в вашем контексте
+
+    cy.get("#sort-order").select("name-desc");
+    cy.wait(1000);
+    // Дополнительная проверка сортировки от Z до A
+    checkSorting("desc", "h2");
+  });
+
+  // Вспомогательная функция для проверки сортировки
+  function checkSorting(order, selector) {
+    cy.get("#product-list > div").then(($products) => {
+      const productNames = $products
+        .toArray()
+        .map((el) => Cypress.$(el).find(selector).text().trim());
+      const sortedProductNames = productNames.slice().sort((a, b) => {
+        if (order === "asc") {
+          return a.localeCompare(b);
+        } else {
+          return b.localeCompare(a);
+        }
+      });
+
+      expect(productNames).to.deep.equal(sortedProductNames);
+    });
+  }
 });
